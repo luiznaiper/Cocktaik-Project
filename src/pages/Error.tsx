732f14ac7df3274aa467cf70c840756835394989
luiznaiper <luiz_naiper@hotmail.com>
2023-07-07 07:56:06 -0600

    :construction: Change js files to ts, create custom.d. for svg and manage Error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/ErrorPage'
+import { Link, useRouteError } from 'react-router-dom'
+import img from '../assets/not-found.svg'
+
+interface IError extends Error {
+  status?: number
+}
 
 const Error = () => {
-  return <div>Error</div>
+  const error = useRouteError() as IError
+
+  if (error instanceof Error) {
+    if (error.status === 404) {
+      return (
+        <Wrapper>
+          <div>
+            <img src={img} alt="not found" />
+            <h3>Ohh!</h3>
+            <p>We can't seem to find page you are looking for</p>
+            <Link to="/">Back Home</Link>
+          </div>
+        </Wrapper>
+      )
+    }
+  }
+  return (
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default Error
