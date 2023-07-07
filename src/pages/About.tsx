45b3eda9b21a329866d0cdece647da3a4dc07925
luiznaiper <luiz_naiper@hotmail.com>
2023-07-07 07:36:40 -0600

    :construction: Add eslint library and Wrapping elements

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/AboutPage'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <Link to="/">Home Page</Link>
-    </div>
+    <Wrapper>
+      <h3>About us</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
+        sequi dolorum laudantium ex nobis. Asperiores, nihil repellendus
+        molestiae quidem facilis esse autem facere. Inventore aspernatur,
+        perferendis reiciendis officiis dolor aliquid. Asperiores similique
+        ullam modi incidunt iure ipsam cumque praesentium quisquam alias
+        reiciendis nesciunt ut omnis fugit dolorum distinctio vero possimus,
+        voluptate facere accusantium a quod? Obcaecati, tenetur. Quisquam,
+        veniam enim.
+      </p>
+    </Wrapper>
   )
 }
 
