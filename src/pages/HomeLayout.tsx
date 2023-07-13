298fd7bdcfeee2412327a9cf71c59ae9f4b01fde
luiznaiper <luiz_naiper@hotmail.com>
2023-07-13 07:28:07 -0600

    :construction: Global loading with useNavigation()

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 const HomeLayout = () => {
+  const navigation = useNavigation()
+  console.log(navigation)
+  const isPageLoading = navigation.state === 'loading'
+
   return (
     <>
       <Navbar />
       <section className="page">
-        <Outlet />
+        {isPageLoading ? <div className="loading"></div> : <Outlet />}
       </section>
     </>
   )
