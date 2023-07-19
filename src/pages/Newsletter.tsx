b54aef73fb1613d30e7d8bfc1101dee995eba643
luiznaiper <luiz_naiper@hotmail.com>
2023-07-19 08:14:07 -0600

    :construction: Add main structure for the newsletter

--- src/pages/Newsletter.tsx
+++ src/pages/Newsletter.tsx
@@ -1,7 +1,59 @@
 import React from 'react'
 
 const Newsletter = () => {
-  return <div>NewsLetter</div>
+  return (
+    <form className="norm">
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        Our Newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="john"
+        />
+      </div>
+      {/* lastName */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="smith"
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: '0.5rem' }}
+      >
+        submit
+      </button>
+    </form>
+  )
 }
 
 export default Newsletter
