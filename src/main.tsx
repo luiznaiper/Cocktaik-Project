1ea97ce31231d395f73e472b72cb34f5ccf32089
luiznaiper <luiz_naiper@hotmail.com>
2023-07-19 08:06:55 -0600

    :construction: Add react toastify to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
+import { ToastContainer } from 'react-toastify'
 
 const rootElement = document.getElementById('root')
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
+      <ToastContainer position="top-center" autoClose={2000} />
       <App />
     </React.StrictMode>
   )
